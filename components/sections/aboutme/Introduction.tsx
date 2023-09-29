91e9ce29ef0d6999f6c86149d414c81f612d1776
popsicle97 <saramdee97@gmail.com>
2023-09-29 19:52:58 +0800

    Remove Intro description

--- components/sections/aboutme/Introduction.tsx
+++ components/sections/aboutme/Introduction.tsx
@@ -1,28 +1,28 @@
 // import ProfilePicture from "../../ProfilePicture";
-import React from "react";
-import { Fonts } from "../../../utils/font";
-import ProfilePicture from "../../ProfilePicture";
+import React from 'react';
+import { Fonts } from '../../../utils/font';
+import ProfilePicture from '../../ProfilePicture';
 
 const Introduction = () => {
-    return (
-        <section id="introduction" className="min-h-screen flex mt-28 flex-row">
-            <div className="flex flex-col max-w-2xl">
-                <span className={`font-mono text-md text-[var(--secondary-color)]`}>
-                    Hi, my name is
-                </span>
-                <h1 className=" text-white font-sans text-6xl leading-[4.5rem] font-bold drop-shadow">
-                    Amery Tan
-                </h1>
-                <span className="font-Cabin text-5xl font-bold text-slate-400 mt-2 mb-3 block">
-                    I am a Web Developer
-                </span>
-                <p className="text-lg mt-4 font-Manrope font-medium text-slate-400 leading-8 drop-shadow">
-                    I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.
-                </p>
-            </div>
-            {/* <ProfilePicture /> */}
-        </section>
-    )
-}
+  return (
+    <section id="introduction" className="mt-28 flex min-h-screen flex-row">
+      <div className="flex max-w-2xl flex-col">
+        <span className={`text-md font-mono text-[var(--secondary-color)]`}>Hi, my name is</span>
+        <h1 className=" font-sans text-6xl font-bold leading-[4.5rem] text-white drop-shadow">
+          Amery Tan
+        </h1>
+        <span className="mt-2 mb-3 block font-Cabin text-5xl font-bold text-slate-400">
+          I am a Web Developer
+        </span>
+        <p className="mt-4 font-Manrope text-lg font-medium leading-8 text-slate-400 drop-shadow">
+          {/* I’m a software engineer specializing in building (and occasionally designing) exceptional
+          digital experiences. Currently, I’m focused on building accessible, human-centered
+          products at Upstatement. */}
+        </p>
+      </div>
+      {/* <ProfilePicture /> */}
+    </section>
+  );
+};
 
 export default Introduction;
